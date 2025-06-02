eff6ba3b8f364d64bc3e6fd2290c2b95d8b21a78
Eray Genc <127109704+eraygencc@users.noreply.github.com>
2025-06-02 11:48:31 +0200

    Update __init__.py

--- dust_extinction/__init__.py
+++ dust_extinction/__init__.py
@@ -1,1 +1,13 @@
-# init.py
+"""
+cirucmgalactic dust_extinction code
+
+This module provides tools to simulate and apply circumgalactic dust extinction effects
+to galaxy magnitudes based on lens-source geometry, halo and stellar masses.
+
+Includes:
+- Dust extinction simulation per lens galaxy.
+- Total extinction calculation per source.
+- Extinction addition to galaxy magnitudes.
+"""
+
+__version__ = "0.1.0"
